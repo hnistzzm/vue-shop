ea1359cafe8a2313fd172dfd1ece0355d2dd06de
zzm <957957209@qq.com>
2021-04-17 14:07:10 +0800

    4.17commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,50 @@
 <template>
   <el-container>
-<!--    头部区域-->
+    <!--    头部区域-->
     <el-header>
       <div>
         <img src="../assets/heima.png" alt="">
-      <span>电商后台管理系统</span>
+        <span>电商后台管理系统</span>
       </div>
       <!--  点击”退出“按钮,将token删除，使用户页面跳转至登录页面-->
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
-<!--    主体区域-->
+    <!--    主体区域-->
     <el-container>
-<!--      侧边栏-->
-<!--      为侧边栏动态的设置宽度，如果点击按钮,则进行侧边栏宽度的转换-->
+      <!--      侧边栏-->
+      <!--      为侧边栏动态的设置宽度，如果点击按钮,则进行侧边栏宽度的转换-->
       <el-aside :width="isCollapse? '64px':'200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
-<!--        侧菜单栏-->
+        <!--        侧菜单栏-->
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b" unique-opened :collapse="isCollapse"
                  :collapse-transition="false" :router="true" :default-active="activePath">
-<!--          一级菜单-->
-<!--          为菜单绑定数据嵌套，获取接口中的数据-->
+          <!--          一级菜单-->
+          <!--          为菜单绑定数据嵌套，获取接口中的数据-->
           <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
-<!--            一级菜单的模板区域-->
+            <!--            一级菜单的模板区域-->
             <template slot="title">
-<!--              图标-->
+              <!--              图标-->
               <i :class="iconsObj[item.id]"></i>
-<!--              文本-->
+              <!--              文本-->
               <span>{{ item.authName }}</span>
             </template>
-<!--            二级菜单-->
-              <el-menu-item :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/'+subItem.path)">
-                <template slot="title">
-                  <!--              图标-->
-                  <i class="el-icon-menu"></i>
-                  <!--              文本-->
-                  <span>{{ subItem.authName }}</span>
-                </template>
+            <!--            二级菜单-->
+            <el-menu-item :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/'+subItem.path)">
+              <template slot="title">
+                <!--              图标-->
+                <i class="el-icon-menu"></i>
+                <!--              文本-->
+                <span>{{ subItem.authName }}</span>
+              </template>
 
-              </el-menu-item>
+            </el-menu-item>
 
           </el-submenu>
 
         </el-menu>
 
       </el-aside>
-<!--      内容主题-->
+      <!--      内容主题-->
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -56,7 +56,7 @@
 <script>
 export default {
   name: 'Home',
-data(){
+  data(){
     return{
       menuList:[],
       //给font里的图标设置一个方法用于调用
@@ -71,7 +71,7 @@ data(){
       //被激活的链接地址
       activePath:''
     }
-},
+  },
   //在登录后马上创建geyMenuList函数用来为之后的获取表单数据做准备
   created () {
     this.getMenuList()
@@ -86,12 +86,12 @@ data(){
       this.$router.push("/login");
     },
     //获取所有的表单
-   async getMenuList(){
-   const {data:res}= await this.$http.get('menus')
-     //200是接口的状态码，如果res.meta.status不等于200,则数据请求成功
-     if(res.meta.status != 200) return this.$message.error(res.meta.msg)
-     this.menuList = res.data
-     console.log(res);
+    async getMenuList(){
+      const {data:res}= await this.$http.get('menus')
+      //200是接口的状态码，如果res.meta.status不等于200,则数据请求成功
+      if(res.meta.status != 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(res);
     },
     //点击按钮，操作菜单的折叠与展开
     toggleCollapse(){
@@ -135,7 +135,7 @@ data(){
 
 }
 .el_main{
- background-color: #cb2020;
+  background-color: #cb2020;
 
 }
 .iconfont{
--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,8 +10,8 @@
   <el-card  class="el-card">
 <!--    查询框布局区域-->
     <el-row :gutter="20">
-      <el-col :span="6">
-        <el-input  placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+      <el-col :span="8">
+        <el-input  placeholder="请输入查询内容" v-model="queryInfo.query" clearable @clear="getUserList">
         <el-button slot="append" icon="el-icon-search" @click="getUserListPage"></el-button>
       </el-input></el-col>
       <el-col :span="4">
@@ -32,11 +32,11 @@
         </template  >
       </el-table-column>
       <el-table-column label="操作" width="200px">
-        <template >
+        <template slot-scope="scope">
 <!--          编辑按钮-->
-          <el-button type="primary" icon="el-icon-edit" size="small"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="small" @click="showEdit(scope.row.id)"></el-button>
 <!--          删除按钮-->
-          <el-button type="danger" icon="el-icon-delete" size="small"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="small" @click="removeUserById(scope.row.id)"></el-button>
 <!--          分配角色按钮-->
           <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
             <el-button type="warning" icon="el-icon-share" size="small"></el-button>
@@ -59,13 +59,13 @@
     <el-dialog
       title="添加用户"
       :visible.sync="addDialogVisible"
-      width="40%">
+      width="40%" @close="addCloseDialog" >
 <!--      内容主体区域-->
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="addForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="username">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="addForm.password"></el-input>
         </el-form-item>
         <el-form-item label="邮箱" prop="email">
@@ -74,15 +74,43 @@
         <el-form-item label="电话" prop="mobile">
           <el-input v-model="addForm.mobile"></el-input>
         </el-form-item>
-
       </el-form>
 
 <!--      底部区域-->
       <span slot="footer" class="dialog-footer">
     <el-button @click="addDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+    <el-button type="primary" @click="addUser">确 定</el-button>
   </span>
     </el-dialog>
+
+
+<!--    修改用户的对话框-->
+    <el-dialog
+      title="修改用户"
+      :visible.sync="editDialogVisible"
+      width="30%" @close="editCloseDialog">
+<!--      内容主体区域-->
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+
+        <span slot="footer" class="dialog-footer">
+    <el-button @click="editDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="editUser()">确 定</el-button>
+  </span>
+    </el-dialog>
+
+<!--    删除用户的对话框-->
+
+
 
 
   </el-card>
@@ -93,17 +121,17 @@
 export default {
   name: 'Users',
   data() {
-    var checkEmail = (rule, value, cb) => {
+    const checkEmail = (rule, value, cb) => {
       const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
       if (regEmail.test(value)) {
-        return cd()
+        return cb()
       }
       cb(new Error('请输入合法的邮箱'))
     }
-    var checkMoblie = (rule, value, cb) => {
-      const regMoblie =  /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
-      if (regMoblie.test(value)) {
-        return cd()
+    const checkMobile = (rule, value, cb) => {
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return cb()
       }
       cb(new Error('请输入合法的电话号码'))
     }
@@ -119,11 +147,15 @@ export default {
       total:0,
       //控制添加用户对话框的显示与隐藏
       addDialogVisible:false,
+      editDialogVisible:false,
       addForm:{
         username:'',
         password:'',
         email:'',
         mobile:''
+      },
+      editForm:{
+
       },
       addFormRules:{
         username:[
@@ -132,7 +164,7 @@ export default {
         ],
         password: [
           {required:true,message:'请输入密码',trigger:'blur'},
-          {min:6,max:12,message: '密码的长度在6~12个字符之间',trigger:'blur'}
+          {min:3,max:12,message: '密码的长度在3~12个字符之间',trigger:'blur'}
         ],
         email: [
           {required:true,message:'请输入邮箱',trigger:'blur'},
@@ -140,7 +172,17 @@ export default {
         ],
        mobile: [
           {required:true,message:'请输入电话号码',trigger:'blur'},
-          {validator:checkMoblie,trigger: 'blur'}
+          {validator:checkMobile,trigger: 'blur'}
+        ]
+      },
+      editFormRules:{
+        email: [
+          {required:true,message:'请输入邮箱',trigger:'blur'},
+          {validator:checkEmail,trigger: 'blur'}
+        ],
+        mobile: [
+          {required:true,message:'请输入电话号码',trigger:'blur'},
+          {validator:checkMobile,trigger: 'blur'}
         ]
       }
     }
@@ -193,7 +235,87 @@ export default {
         return this.$message.error('更新用户状态失败!')
       }
       this.$message.success('更新用户状态成功！')
+    },
+    //关闭表单后，将表单里的内容清空
+    addCloseDialog(){
+      this.$refs.addFormRef.resetFields();
+    },
+    //进行与后台的验证，如果验证成功，则添加用户成功
+    addUser(){
+      this.$refs.addFormRef.validate(async valid =>{
+        if(!valid) return
+        //console.log(valid)
+       const {data:res}= await this.$http.post('users',this.addForm)
+        if(res.meta.status!==201)
+          this.$message.error('添加用户失败！')
+        else
+        this.$message.success('添加用户成功！')
+        //将对话框隐藏
+        this.addDialogVisible = false
+        //重置表单
+        await this.getUserList()
+      } )
+    },
+    editUser(){
+      this.$refs.editFormRef.validate(async valid =>{
+        if(!valid) return
+        console.log(valid)
+        //发起修改用户信息的数据请求
+        const {data:res}= await  this.$http.put("users/"+this.editForm.id,{
+          email:this.editForm.email,
+          mobile:this.editForm.mobile
+        })
+        if(res.meta.status!==200)
+          this.$message.error('修改用户信息失败！')
+        else
+          this.$message.success('修改用户信息成功！')
+        this.editDialogVisible =false
+        await  this.getUserList()
+
+      })
+    },
+    //显示编辑用户的对话框
+    async showEdit(id){
+
+      //console.log(id)
+      const {data:res} = await this.$http.get("users/"+id)
+      if(res.meta.status!==200)
+        return this.$message.error('查询用户信息失败!')
+      this.editForm = res.data
+      this.editDialogVisible =true
+
+    },
+    //重置编辑表单
+    editCloseDialog(){
+      this.$refs.editFormRef.resetFields();
+    },
+    //显示删除用户的对话框
+    async removeUserById(id){
+      //const {data:res} = await this.$http.delete("users/"+id)
+    //console.log(id)
+
+     const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      //如果用户确定删除，则返回值为字符串confirm
+      //如果用户取消了删除，则返回值为字符串cancel
+      //console.log(confirmResult)
+      if(confirmResult !=='confirm'){
+        return this.$message.info('已取消删除')
+      }
+      //console.log('确认了删除')
+      const {data:res} = await this.$http.delete("users/"+id)
+      if(res.meta.status!==200){
+         this.$message.error('删除失败!')
+      }
+      else
+        this.$message.success('删除成功!')
+      //重置表单
+      await  this.getUserList()
     }
+
   }
 
 }
